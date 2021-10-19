800fe0b2e52b00e4b1280da7f900e40f91a71d5f
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-18 23:22:51 -0300

    improved the regex of essential_packages.py

--- tests/essential_packages.py
+++ tests/essential_packages.py
@@ -15,13 +15,13 @@ packages_list = os.listdir(repos)
 
 @pytest.mark.parametrize('package', mate_iso_packages)
 def test_01_verify_ghostbsd_packages_iso_exist(package):
-    regex = re.compile(f'({package}-)([0-9]|v).+')
+    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
     found = list(filter(regex.match, packages_list))
     assert found, f'{package} is missing'
 
 
 @pytest.mark.parametrize('package', xfce_iso_packages)
 def test_02_verify_ghostbs_xfce_packages_iso_exist(package):
-    regex = re.compile(f'({package}-)([0-9]|v).+')
+    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
     found = list(filter(regex.match, packages_list))
     assert found, f'{package} is missing'
