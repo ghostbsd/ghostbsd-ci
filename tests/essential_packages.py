999343eec1ce4559054b57d29cad0f9c1f91d007
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-22 07:40:51 -0300

    Added OS packages verification in essential_packages.py

--- tests/essential_packages.py
+++ tests/essential_packages.py
@@ -10,18 +10,31 @@ raw_url = 'https://raw.githubusercontent.com/ghostbsd/ghostbsd-build' \
     '/master/packages'
 mate_iso_packages = requests.get(f'{raw_url}/mate').text.split()
 xfce_iso_packages = requests.get(f'{raw_url}/xfce').text.split()
+iso_os_packages = [
+    "os-generic-kernel",
+    "os-generic-userland",
+    "os-generic-userland-lib32",
+    "os-generic-userland-devtools"
+]
 packages_list = os.listdir(repos)
 
 
+@pytest.mark.parametrize('package', iso_os_packages)
+def test_01_verify_ghostbsd_iso_os_packages_exists(package):
+    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
+    found = list(filter(regex.match, packages_list))
+    assert found, f'{package} is missing'
+
+
 @pytest.mark.parametrize('package', mate_iso_packages)
-def test_01_verify_ghostbsd_packages_iso_exist(package):
+def test_02_verify_ghostbsd_packages_iso_exists(package):
     regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
     found = list(filter(regex.match, packages_list))
     assert found, f'{package} is missing'
 
 
 @pytest.mark.parametrize('package', xfce_iso_packages)
-def test_02_verify_ghostbs_xfce_packages_iso_exist(package):
+def test_03_verify_ghostbs_xfce_packages_iso_exists(package):
     regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
     found = list(filter(regex.match, packages_list))
     assert found, f'{package} is missing'
