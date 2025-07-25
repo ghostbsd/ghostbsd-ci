b7908f5ba163e3f0f2266bba694793fa883a4f17
ericbsd <ericturgeon.bsd@gmail.com>
2025-07-25 17:56:22 -0300

    refactored essential_packages test to use environment variable for versioning and simplified test logic
    - Replaced argparse with environment variable for version management
    - Consolidated package existence checks into a reusable function
    - Updated pipeline to align with environment-based versioning
    - Adjusted test dependencies to Python 3.11

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
 
-packages = 'py39-requests py39-pytest'
+packages = 'py311-requests py311-pytest'
 
 os.system(f'pkg install -y {packages}')
--- tests/essential_packages.py
+++ tests/essential_packages.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
-
-import argparse
 import os
 import pytest
 import re
 import requests
-import sys
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Test GhostBSD package availability')
-parser.add_argument('--version', default='14', help='Repository version (default: 14)')
-args, unknown = parser.parse_known_args()
+# Get version from environment variable, default to '14'
+version = os.environ.get('GHOSTBSD_VERSION', '14')
 
-# Remove our arguments from sys.argv so pytest doesn't see them
-sys.argv = [sys.argv[0]] + unknown
-
-repos = f'/usr/local/poudriere/data/packages/ghostbsd-{args.version}-ghostbsd_ports/.latest/All'
+repos = f'/usr/local/poudriere/data/packages/ghostbsd-{version}-ghostbsd_ports/.latest/All'
 raw_url = 'https://raw.githubusercontent.com/ghostbsd/ghostbsd-build/master/packages'
 mate_iso_packages = requests.get(f'{raw_url}/mate').text.split()
 xfce_iso_packages = requests.get(f'{raw_url}/xfce').text.split()
@@ -32,43 +24,37 @@ packages_list = os.listdir(repos)
 useful_packages = open('tests/useful_packages', 'r').read().splitlines()
 
 
-@pytest.mark.parametrize('package', mate_iso_packages)
-def test_verify_ghostbsd_packages_iso_exists(package):
+def verify_package_exists(package):
     regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
     found = list(filter(regex.match, packages_list))
     assert found, f'{package} is missing'
+
+
+@pytest.mark.parametrize('package', mate_iso_packages)
+def test_verify_ghostbsd_packages_iso_exists(package):
+    verify_package_exists(package)
 
 
 @pytest.mark.parametrize('package', xfce_iso_packages)
 def test_verify_ghostbsd_xfce_packages_iso_exists(package):
-    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
-    found = list(filter(regex.match, packages_list))
-    assert found, f'{package} is missing'
+    verify_package_exists(package)
 
 
 @pytest.mark.parametrize('package', gershwin_iso_packages)
 def test_verify_ghostbsd_gershwin_packages_iso_exists(package):
-    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
-    found = list(filter(regex.match, packages_list))
-    assert found, f'{package} is missing'
+    verify_package_exists(package)
 
 
 @pytest.mark.parametrize('package', common_iso_packages)
 def test_verify_ghostbsd_common_packages_iso_exists(package):
-    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
-    found = list(filter(regex.match, packages_list))
-    assert found, f'{package} is missing'
+    verify_package_exists(package)
 
 
 @pytest.mark.parametrize('package', drivers_iso_packages)
 def test_verify_ghostbsd_drivers_packages_iso_exists(package):
-    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
-    found = list(filter(regex.match, packages_list))
-    assert found, f'{package} is missing'
+    verify_package_exists(package)
 
 
 @pytest.mark.parametrize('package', useful_packages)
 def test_verify_ghostbsd_useful_packages_exists(package):
-    regex = re.compile(f'({package}-)([0-9]|v[0-9]|g[0-9]).+')
-    found = list(filter(regex.match, packages_list))
-    assert found, f'{package} is missing'
+    verify_package_exists(package)
