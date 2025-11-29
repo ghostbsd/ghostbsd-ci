36e0696a1194b124b5c341a4faa17b66060bcf2d
ericbsd <ericturgeon.bsd@gmail.com>
2025-11-29 15:13:37 -0400

    added support for MATE OEM package group in essential_packages test
    - Included temporary test for verifying MATE OEM package existence
    - Removed unused `iso_os_packages` list
    - Improved test logic and organization

--- tests/essential_packages.py
+++ tests/essential_packages.py
@@ -10,16 +10,11 @@ version = os.environ.get('GHOSTBSD_VERSION', '14')
 repos = f'/usr/local/poudriere/data/packages/ghostbsd-{version}-ghostbsd_ports/.latest/All'
 raw_url = 'https://raw.githubusercontent.com/ghostbsd/ghostbsd-build/master/packages'
 mate_iso_packages = requests.get(f'{raw_url}/mate').text.split()
+mate_oem_iso_packages = requests.get(f'{raw_url}/mate_oem').text.split()
 xfce_iso_packages = requests.get(f'{raw_url}/xfce').text.split()
 gershwin_iso_packages = requests.get(f'{raw_url}/gershwin').text.split()
 common_iso_packages = requests.get(f'{raw_url}/common').text.split()
 drivers_iso_packages = requests.get(f'{raw_url}/drivers').text.split()
-iso_os_packages = [
-    "os-generic-kernel",
-    "os-generic-userland",
-    "os-generic-userland-lib32",
-    "os-generic-userland-devtools"
-]
 packages_list = os.listdir(repos)
 useful_packages = open('tests/useful_packages', 'r').read().splitlines()
 
@@ -31,6 +26,12 @@ def verify_package_exists(package):
 
 
 @pytest.mark.parametrize('package', mate_iso_packages)
+def test_verify_ghostbsd_packages_iso_exists(package):
+    verify_package_exists(package)
+
+
+# Todo: this is temporary, remove when mate_oem is removed.
+@pytest.mark.parametrize('package', mate_oem_iso_packages)
 def test_verify_ghostbsd_packages_iso_exists(package):
     verify_package_exists(package)
 
